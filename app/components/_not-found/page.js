adad5ac88d07dcc7727a697dd3aef8b3797a113d
grahamusai <53958849+grahamusai@users.noreply.github.com>
2025-01-20 16:10:29 +0200

    build errors

---
+++ app/components/_not-found/page.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <h1 className="text-6xl font-bold">404 - Page Not Found</h1>
+    </div>
+  );
+}
--- app/components/navbar.js
+++ app/components/navbar.js
@@ -4,6 +4,7 @@ import { Search } from 'lucide-react'
 import Image from "next/image"
 import Link from "next/link"
 
+
 import { Button } from "../../components/ui/button";
 import {
   DropdownMenu,
